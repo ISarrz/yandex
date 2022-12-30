648361f78261f4e83940127c119c694c1d7177b9
German Zlobin <germanzlobin2@gmail.com>
2022-12-30 14:53:48 +0700

    фикс клавиш +изображение игрока

--- 1.py
+++ 1.py
@@ -19,10 +19,10 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Hero(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, group, pos):
         super().__init__(group)
-        self.image = load_image("mario.png")
+        self.image = load_image("player_1.png")
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
@@ -60,7 +60,7 @@ gaming = True
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('WHITE'))
 all_sprites = pygame.sprite.Group()
-hero = Hero(all_sprites, (10, 10))
+player = Player(all_sprites, (10, 10))
 all_sprites.draw(screen)
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
@@ -69,37 +69,25 @@ Border(5, size[1] - 5, size[0] - 5, size[1] - 5)
 Border(5, 5, 5, size[1] - 5)
 Border(size[0] - 5, 5, size[0] - 5, size[1] - 5)
 pygame.display.flip()
-fps = 50  # количество кадров в секунду
+fps = 60  # количество кадров в секунду
 clock = pygame.time.Clock()
 cont = False
+player_speed = 3
 while gaming:
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        player.update((0, -player_speed))
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        player.update((0, player_speed))
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        player.update((-player_speed, 0))
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        player.update((player_speed, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gaming = False
-        if event.type == pygame.KEYDOWN:
-            new_key = event.key
-            if new_key == pygame.K_UP:
-                hero.update((0, -1))
-            if new_key == pygame.K_DOWN:
-                hero.update((0, 1))
-            if new_key == pygame.K_LEFT:
-                hero.update((-1, 0))
-            if new_key == pygame.K_RIGHT:
-                hero.update((1, 0))
-            last_key = new_key
-            cont = True
-        if event.type == pygame.KEYUP:
-            cont = False
-    if cont:
-        print('go')
-        if last_key == pygame.K_UP:
-            hero.update((0, -1))
-        if last_key == pygame.K_DOWN:
-            hero.update((0, 1))
-        if last_key == pygame.K_LEFT:
-            hero.update((-1, 0))
-        if last_key == pygame.K_RIGHT:
-            hero.update((1, 0))
+        
+
     screen.fill(pygame.Color('WHITE'))
     all_sprites.draw(screen)
     pygame.display.flip()
