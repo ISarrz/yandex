3295cc6a47f08aec43dedef0af4fc6b12e732fde
Danil <Danilshkr@gmail.com>
2023-01-09 13:27:03 +0700

    shooting

--- 1.py
+++ 1.py
@@ -22,46 +22,120 @@ def load_image(name, colorkey=-1):
 class Player(pygame.sprite.Sprite):
     def __init__(self, group, pos):
         super().__init__(group)
-        self.image = load_image("player_right.png")
+        self.image = load_image("player_right1.png")
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
         self.last_move = [0, 0]
+        self.last_move2 = [0, 0]
+        self.speed_shooting = 10
+        self.now_image = ['right', 'player_right1.png']
+        self.images = {
+            'down': ['player_down1.png', 'player_down2.png'],
+            'up': ['player_up1.png', 'player_up2.png'],
+            'right': ['player_right1.png', 'player_right2.png'],
+            'left': ['player_left1.png', 'player_left2.png']
+        }
 
     def update(self, new_pos):
-        if self.last_move[0] != new_pos[0] or self.last_move[1] != new_pos[1]:
-            self.change_image(new_pos)
+        if self.last_move2[0] != new_pos[0] or self.last_move2[1] != new_pos[1]:
+            self.change_image_for_moving(new_pos)
+            print('no')
+        else:
+            print('ok')
+            self.animation_moving()
         if new_pos[1] != 0:
             if not pygame.sprite.spritecollideany(self, horizontal_borders):
                 self.rect.y += new_pos[1]
                 self.last_move[1] = new_pos[1]
+                self.last_move2 = new_pos
             else:
                 self.rect.y -= self.last_move[1]
         if new_pos[0] != 0:
             if not pygame.sprite.spritecollideany(self, vertical_borders):
                 self.rect.x += new_pos[0]
                 self.last_move[0] = new_pos[0]
+                self.last_move2 = new_pos
             else:
                 self.rect.x -= self.last_move[0]
 
-
-    def change_image(self, new_pos):
-        if new_pos[0] > 0:
-            self.image = load_image('player_right.png')
-        if new_pos[0] < 0:
+    def animation_stop(self):
+        if self.now_image[0] == 'left':
             self.image = load_image('player_left.png')
-        if new_pos[1] > 0:
+        if self.now_image[0] == 'right':
+            self.image = load_image('player_right.png')
+        if self.now_image[0] == 'down':
             self.image = load_image('player_down.png')
-        if new_pos[1] < 0:
+        if self.now_image[0] == 'up':
             self.image = load_image('player_up.png')
+
+    def animation_moving(self):
+        if self.now_image[1][-5] == '2':
+            self.image = load_image(self.images[self.now_image[0]][0])
+            self.now_image = [self.now_image[0], self.images[self.now_image[0]][0]]
+        else:
+            self.image = load_image(self.images[self.now_image[0]][1])
+            self.now_image = [self.now_image[0], self.images[self.now_image[0]][1]]
         x = self.rect.x
         y = self.rect.y
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
+    def change_image_for_moving(self, new_pos):
+        if new_pos[0] > 0:
+            self.image = load_image('player_right1.png')
+            self.now_image = ['right', 'player_right1.png']
+        if new_pos[0] < 0:
+            self.image = load_image('player_left1.png')
+            self.now_image = ['left', 'player_left1.png']
+        if new_pos[1] > 0:
+            self.image = load_image('player_down1.png')
+            self.now_image = ['down', 'player_down1.png']
+        if new_pos[1] < 0:
+            self.image = load_image('player_up1.png')
+            self.now_image = ['up', 'player_up1.png']
+        x = self.rect.x
+        y = self.rect.y
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-    #def strike(self, position):
+    def strike(self):
+        if self.now_image[0] == 'right':
+            #self.image = load_image('player_right_s.png')
+            pulya.append(Shooting(all_sprites, (self.rect.x, self.rect.y), (self.speed_shooting, 0)))
+        if self.now_image[0] == 'left':
+            #self.image = load_image('player_left_s.png')
+            pulya.append(Shooting(all_sprites, (self.rect.x, self.rect.y), (-self.speed_shooting, 0)))
+        if self.now_image[0] == 'down':
+            #self.image = load_image('player_down_s.png')
+            pulya.append(Shooting(all_sprites, (self.rect.x, self.rect.y), (0, self.speed_shooting)))
+        if self.now_image[0] == 'up':
+            #self.image = load_image('player_up_s.png')
+            pulya.append(Shooting(all_sprites, (self.rect.x, self.rect.y), (0, -self.speed_shooting)))
+        pulya_group.add(pulya[-1])
+
+
+class Shooting(pygame.sprite.Sprite):
+    def __init__(self, group, pos, where):
+        super().__init__(group)
+        self.image = load_image('pulya.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1] + 20
+        self.where = where
+
+    def update(self):
+        print(self.rect.center)
+        if not pygame.sprite.spritecollideany(self, horizontal_borders) and not pygame.sprite.spritecollideany(self, vertical_borders):
+            self.rect.x += self.where[0]
+            self.rect.y += self.where[1]
+        else:
+            delet = pulya.pop(pulya.index(self))
+            all_sprites.remove(delet)
+            pulya_group.remove(delet)
+
 
 
 
@@ -112,26 +186,39 @@ Border(5, size[1] - 5, size[0] - 5, size[1] - 5)
 Border(5, 40, 5, size[1] - 5)
 Border(size[0] - 5, 40, size[0] - 5, size[1] - 5)
 health = []
+pulya_group = pygame.sprite.Group()
+pulya = []
 x, y = 10, 10
 for _ in range(10):
     health.append(Health((x, y)))
     x += 30
 print(health)
 pygame.display.flip()
-fps = 60  # количество кадров в секунду
+fps = 110  # количество кадров в секунду
 clock = pygame.time.Clock()
 cont = False
 player_speed = 3
 while gaming:
     keys = pygame.key.get_pressed()
+    check = True
     if keys[pygame.K_UP] or keys[pygame.K_w]:
         player.update((0, -player_speed))
+        check = False
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
         player.update((0, player_speed))
+        check = False
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         player.update((-player_speed, 0))
+        check = False
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.update((player_speed, 0))
+        check = False
+    """
+    if keys[pygame.BUTTON_LEFT]:
+        player.strike()
+    """
+    if check:
+        player.animation_stop()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gaming = False
@@ -140,13 +227,9 @@ while gaming:
                 gaming = health[-1].update(0)
             if event.key == pygame.K_EQUALS:
                 gaming = health[-1].update(1)
-        """if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.btn == pygame.BUTTON_LEFT:
-                pos = event.pos
-                if pos
+            if event.key == pygame.K_SPACE:
                 player.strike()
-                """
-
+    pulya_group.update()
     screen.fill(pygame.Color('WHITE'))
     all_sprites.draw(screen)
     pygame.display.flip()
